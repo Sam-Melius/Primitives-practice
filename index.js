74b8482beef1a34f08a41396e7be21272ec7132d
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-04 12:38:55 -0700

    challenge #3 complete

--- index.js
+++ index.js
@@ -26,3 +26,16 @@ function oddishOrEvenish(number) {
   }
   console.log(oddishOrEvenish(121));
   console.log(oddishOrEvenish(41));
+
+  // Challenge #3 Fizz Buzz
+function fizzBuzz(number) {
+    let arr = [];
+    for (let i = 1; i < 16; i++) {
+      if (i % 15 === 0) arr.push('FizzBuzz');
+      else if (i % 3 === 0) arr.push('Fizz');
+      else if (i % 5 === 0) arr.push('Buzz');
+      else arr.push(i);
+    }
+    return arr;
+  }
+  console.log(fizzBuzz(16));
