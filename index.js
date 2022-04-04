350bedc87f0c6535a0dd18ced97aa35a5c681da3
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-04 12:59:12 -0700

    rewrote challenge #3 to be cleaner

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ function oddishOrEvenish(number) {
   // Challenge #3 Fizz Buzz
 function fizzBuzz(number) {
     let arr = [];
-    for (let i = 1; i < 16; i++) {
-      if (i % 15 === 0) arr.push('FizzBuzz');
+    for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 && i % 5 ===0) arr.push('FizzBuzz');
       else if (i % 3 === 0) arr.push('Fizz');
       else if (i % 5 === 0) arr.push('Buzz');
       else arr.push(i);
